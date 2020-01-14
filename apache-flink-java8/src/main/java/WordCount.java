ecf1fd95bf5a2f40bff2d0c68b0e1335d2fb20d5
yashsugandh <7276363377>
2020-01-14 17:25:57 +0530

    apache-flink-java-8-lambdas files added

---
+++ apache-flink-java8/src/main/java/WordCount.java
@@ -0,0 +1,29 @@
+import org.apache.flink.api.java.DataSet;
+import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.util.Collector;
+
+import java.util.Arrays;
+
+public class WordCount {
+
+  public static void main(String[] args) throws Exception {
+
+    ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+
+    DataSet<String> input = env.fromElements("Please count", "the words", "but not this");
+
+    // filter out strings that contain "not"
+    input.filter(line -> !line.contains("not"))
+        // split each line by space
+        .map(line -> line.split(" "))
+        // emit a pair <word,1> for each array element
+        .flatMap(
+            (String[] wordArray, Collector<Tuple2<String, Integer>> out) -> Arrays.stream(wordArray)
+                .forEach(t -> out.collect(new Tuple2<>(t, 1))))
+        // group and sum up
+        .groupBy(0).sum(1)
+        // print
+        .print();
+  }
+}
