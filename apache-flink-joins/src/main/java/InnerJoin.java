4fb41b35a9da9cf2b9583ada4a3e8726b381a808
yashsugandh <7276363377>
2020-01-15 10:59:34 +0530

    Adding Different types of joins in flink

---
+++ apache-flink-joins/src/main/java/InnerJoin.java
@@ -0,0 +1,51 @@
+import org.apache.flink.api.common.functions.JoinFunction;
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.java.DataSet;
+import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.api.java.tuple.Tuple3;
+import org.apache.flink.api.java.utils.ParameterTool;
+
+public class InnerJoin {
+
+  public static final String PARALLELISM = "parallelism";
+
+  public static void main(String[] args) throws Exception {
+
+    final ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+
+    final ParameterTool parameterTool = ParameterTool.fromArgs(args);
+
+    // To get only one file as output
+    env.setParallelism(parameterTool.getInt(PARALLELISM));
+
+    final DataSet<Tuple2<Integer, String>> personSet = env.readTextFile(parameterTool.get("input1"))
+        .map(new MapFunction<String, Tuple2<Integer, String>>() {
+          public Tuple2<Integer, String> map(String value) {
+            String[] words = value.split(",");
+            return new Tuple2<Integer, String>(Integer.parseInt(words[0]), words[1]);
+          }
+        });
+
+    DataSet<Tuple2<Integer, String>> locationSet = env.readTextFile(parameterTool.get("input2"))
+        .map(new MapFunction<String, Tuple2<Integer, String>>() {
+          public Tuple2<Integer, String> map(String value) {
+            String[] words = value.split(",");
+            return new Tuple2<Integer, String>(Integer.parseInt(words[0]), words[1]);
+          }
+        });
+
+    DataSet<Tuple3<Integer, String, String>> joined = personSet.join(locationSet).where(0)
+        .equalTo(0).with(
+            new JoinFunction<Tuple2<Integer, String>, Tuple2<Integer, String>, Tuple3<Integer, String, String>>() {
+              public Tuple3<Integer, String, String> join(Tuple2<Integer, String> person,
+                  Tuple2<Integer, String> location) {
+                return new Tuple3<Integer, String, String>(person.f0, person.f1, location.f1);
+              }
+            });
+
+    joined.writeAsCsv(parameterTool.get("output"), "\n", " ");
+
+    env.execute("Join Example");
+  }
+}
